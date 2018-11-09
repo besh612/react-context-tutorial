32aedb7bb7fa00a29a5b8dbd087911cbdcf91933
besh612 <sung.hun5016@gmail.com>
2018-11-09 22:48:39 +0900

    delete unused code in Recevies component

--- src/component/Receives.js
+++ src/component/Receives.js
@@ -1,22 +1,12 @@
 import React from "react";
-import PropTypes from "prop-types";
-
 import NewContext from "../context/NewContext";
 
 const Receives = () => {
   return (
     <NewContext.Consumer>
-      {({ input }) => (
-        <div>
-          현재 값 : {input} {console.log("input :", input)}
-        </div>
-      )}
+      {context => <div>현재 설정된 값: {context}</div>}
     </NewContext.Consumer>
   );
-};
-
-Receives.contextTypes = {
-  input: PropTypes.object // eslint-disable-line
 };
 
 export default Receives;
