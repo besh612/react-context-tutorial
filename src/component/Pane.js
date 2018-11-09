0157867e174fa2f4ff06b5c411868448115ece76
besh612 <sung.hun5016@gmail.com>
2018-11-09 22:47:05 +0900

    add child component for test context

---
+++ src/component/Pane.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Receives from "./Receives";
+
+const Pane = () => {
+  return (
+    <div className="pane">
+      <Receives />
+    </div>
+  );
+};
+
+export default Pane;
